23c3440d58ac08811dedbbfcbcf3a6f21e1517df
Gleidson Jonas <gleidsonjonas@hotmail.com>
2024-05-08 22:06:59 -0300

    feat: add base api request

--- mangue-bem-web/src/components/Map.tsx
+++ mangue-bem-web/src/components/Map.tsx
@@ -4,28 +4,28 @@ import "../styles/Map.css";
 /*global simplemaps_countrymap_mapdata */
 // @ts-ignore
 
-export const return_estado_name_to_id = () => {
-  const estado_name_to_id = {};
-  for (let id in simplemaps_countrymap_mapdata.state_specific) {
-    //console.log(id);
-    //const name = simplemaps_countrymap_mapdata.state_specific[id].name;
-    estado_name_to_id[
-      simplemaps_countrymap_mapdata.state_specific[id].name.toString()
-    ] = id;
-    //const code = id.slice(0, 3);
-  }
-  return estado_name_to_id;
-};
+// export const return_estado_name_to_id = () => {
+//   const estado_name_to_id = {};
+//   for (let id in simplemaps_countrymap_mapdata.state_specific) {
+//     //console.log(id);
+//     //const name = simplemaps_countrymap_mapdata.state_specific[id].name;
+//     estado_name_to_id[
+//       simplemaps_countrymap_mapdata.state_specific[id].name.toString()
+//     ] = id;
+//     //const code = id.slice(0, 3);
+//   }
+//   return estado_name_to_id;
+// };
 
-export const return_state_names = () => {
-  const state_names = {};
-  for (let id in simplemaps_countrymap_mapdata.state_specific) {
-    state_names.push(
-      simplemaps_countrymap_mapdata.state_specific[id].name.toString()
-    );
-  }
-  return state_names;
-};
+// export const return_state_names = () => {
+//   const state_names = {};
+//   for (let id in simplemaps_countrymap_mapdata.state_specific) {
+//     state_names.push(
+//       simplemaps_countrymap_mapdata.state_specific[id].name.toString()
+//     );
+//   }
+//   return state_names;
+// };
 
 function Map() {
   return (
